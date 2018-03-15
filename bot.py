4171ed77c38af5d0b71c0f9a57a17644dd5c1b6c
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-15 00:02:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,11 @@ class Twitch_Bot(tcommands.TwitchBot):
   def __init__(self):
     super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'])
     
-  async def test(ctx):
+  @tcommands.twitch_command()
+  async def test(self, ctx):
     await ctx.send('I am alive')
+    
+Twitch_Bot().run()
+
+bot = dcommands.Bot(command_prefix=when_command_mentioned.or('!'))
+bot.run(private.DISCORD_TOKEN)
