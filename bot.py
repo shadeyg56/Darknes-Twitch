0374350a3967768107ce008be1d626e625112e47
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-15 00:26:16 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,20 @@ class Twitch_Bot(tcommands.TwitchBot):
   async def test(self, ctx):
     await ctx.send('I am alive')
     
+  async def event_ready(self):
+    print('Logged into Twitch')
+    
 Twitch_Bot().run()
 
 bot = dcommands.Bot(command_prefix=dcommands.when_command_mentioned_or('!'))
+@bot.event()
+async def on_ready():
+  print('------------------------------------')
+  print('THE BOT IS ONLINE')
+  print('------------------------------------')
+  print("Name: {}".format(bot.user.name))
+  print('Author: shadeyg56')
+  print("ID: {}".format(bot.user.id))
+  print('DV: {}'.format(discord.__version__))
+  await bot.change_presence(activity=discord.Streaming(name='watching over the chat until next stream', url='https://www.twitch.tv/shadeyg56'))
 bot.run(private.DISCORD_TOKEN)
