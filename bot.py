c1061604fcca9c9d7354b8e268a67304c77d5960
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-15 00:38:21 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands as dcommands
 from twitchio import commands as tcommands
 import private
+import asyncio
 
 class Twitch_Bot(tcommands.TwitchBot):
   def __init__(self):
@@ -16,15 +17,23 @@ class Twitch_Bot(tcommands.TwitchBot):
     
 Twitch_Bot().run()
 
-bot = dcommands.Bot(command_prefix=dcommands.when_command_mentioned_or('!'))
-@bot.event()
-async def on_ready():
-  print('------------------------------------')
-  print('THE BOT IS ONLINE')
-  print('------------------------------------')
-  print("Name: {}".format(bot.user.name))
-  print('Author: shadeyg56')
-  print("ID: {}".format(bot.user.id))
-  print('DV: {}'.format(discord.__version__))
-  await bot.change_presence(activity=discord.Streaming(name='watching over the chat until next stream', url='https://www.twitch.tv/shadeyg56'))
-bot.run(private.DISCORD_TOKEN)
+async def run():
+  bot = Bot
+  bot.start(private.DISCORD_TOKEN
+
+class Bot(dcommands.Bot):
+  def __init__(self):
+    super().__init__(command_prefix=dcommands.when_command_mentioned_or('!'))
+    
+  async def on_ready():
+      print('------------------------------------')
+      print('THE BOT IS ONLINE')
+      print('------------------------------------')
+      print("Name: {}".format(bot.user.name))
+      print('Author: shadeyg56')
+      print("ID: {}".format(bot.user.id))
+      print('DV: {}'.format(discord.__version__))
+      await bot.change_presence(activity=discord.Streaming(name='watching over the chat until next stream', url='https://www.twitch.tv/shadeyg56'))
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+
