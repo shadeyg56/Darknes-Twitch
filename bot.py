da11e6ef8221be7c124d45d708a404c7cf606663
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-14 23:25:10 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
 import discord
 from discord.ext import commands as dcommands
 from twitchio import commands as tcommands
+import private
 
-class Twitch_Bot(self):
+class Twitch_Bot(tcommands.TwitchBot):
+  def __init__(self):
+    super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'])
+    
+  async def test(ctx):
+    await ctx.send('I am alive')
