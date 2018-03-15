1d8d8adb49b725fb295be894ff1650029a7ea75e
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-15 12:15:20 -0500

    Update and rename start_bots.sh to start_bots.py

---
+++ start_bots.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands as dcommands
+from twitchio import commands as tcommands
+import bot
+import twitch
+
+#calls startup functions from bots
+bot.start()
+twith.start()
+
--- start_bots.sh
+++
@@ -1,2 +0,0 @@
-/home/pi/Desktop/Darkness-Twitch/bot.py
-/hone/pi/Desktop/Darkness-Twitch/twitch.py
