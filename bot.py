ecacbfd14aa4385b36e90615a2454026a551ff8b
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-15 09:42:48 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,15 +16,11 @@ async def on_ready():
   print("ID: {}".format(bot.user.id))
   print('DV: {}'.format(discord.__version__))
   await bot.change_presence(activity=discord.Streaming(name='Watching over the chat until next stream', url='https://www.twitch.tv/shadeyg56'))
-
-async def load_twitch():
-  await bot.wait_until_ready()
+  
+try:
   bot.load_extension('twitch')
-  try:
-    print('Loaded: twitch')
-  except Exception as e:
-    print(f'Error: {e}')
+  print('Loaded: twitch')
+except Exception as e:
+  print(f'Error: {e}')
 
 bot.run(private.DISCORD_TOKEN)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(load_twitch())
