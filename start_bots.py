b5bc826dcab77ee36ae74a76a6267a938ceb3dfc
shadeyg56 <shadeyg56@gmail.com>
2018-03-15 12:58:19 -0500

    ...

--- start_bots.py
+++ start_bots.py
@@ -6,7 +6,7 @@ import subprocess
 scripts = ['/home/pi/Desktop/Darkness-Twitch/bot.py', 'home/pi/Desktop/Darkness-Twitch/twitch.py']
 for script in scripts:
   #calls startup functions from bots
-  p = subprocess.Popen([script, "ArcView"], shell=True, stdin=PIPE, stdout=PIPE)
+  p = subprocess.Popen([script, "ArcView"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
   output = p.communicate()
   print(output[0])
 
