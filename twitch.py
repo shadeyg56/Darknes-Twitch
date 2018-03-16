756ee74b8384ce51aed0cc716648947a15211fed
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-16 11:55:13 -0500

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -4,9 +4,10 @@ from discord.ext import commands as dcommands
 import private
     
 class Twitch_Bot(tcommands.TwitchBot):
-  def __init__(self):
+  def __init__(self, bot):
     super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'])
-  
+    self.bot = bot
+    
   @tcommands.twitch_command()
   async def test(self, ctx):
     await ctx.send('I am alive')
@@ -14,5 +15,6 @@ class Twitch_Bot(tcommands.TwitchBot):
   async def event_ready(self):
     print('Logged into Twitch')
     
-    
+def setup(bot):
+    bot.add_cog(Twitch_Bot(bot)
 Twitch_Bot().run()
