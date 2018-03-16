74a4c63b2838aa15b5cd725a1207a6b1b98fb1c8
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-03-16 16:37:19 -0500

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -2,7 +2,7 @@ from twitchio import commands as tcommands
 import discord
 from discord.ext import commands as dcommands
 import private
-    
+from dcommands import Bot as bot
 class Twitch_Bot(tcommands.TwitchBot):
   def __init__(self):
     super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'])
@@ -10,6 +10,7 @@ class Twitch_Bot(tcommands.TwitchBot):
   @tcommands.twitch_command()
   async def test(self, ctx):
     await ctx.send('I am alive')
+    await bot.change_presence(activity=discord.Game(name='testing succesful!'))
     
   async def event_ready(self):
     print('Logged into Twitch')
