eeaec05a1830478c1a7cce53d1181748fa7b7aea
shadeyg56 <shadeyg56@gmail.com>
2018-03-17 17:05:57 -0500

    add client id

--- twitch.py
+++ twitch.py
@@ -7,7 +7,7 @@ import asyncio
 
 class Twitch_Bot(tcommands.TwitchBot):
   def __init__(self):
-    super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'])
+    super().__init__(prefix='!', nick='Darkness', token=private.TWITCH_TOKEN, initial_channels=['shadeyg56'], client_id=private.TWITCH_CLIENT_ID)
     self.loop = asyncio.get_event_loop()
     self.loop.create_task(self.live())
     self.loop.run_forever()
