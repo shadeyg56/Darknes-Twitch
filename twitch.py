1988df8eb0486344d1c76def9cad26d87270caa5
shadeyg56 <shadeyg56@gmail.com>
2018-03-18 19:08:54 -0500

    this sucks

--- twitch.py
+++ twitch.py
@@ -49,7 +49,7 @@ class Twitch_Bot(tcommands.TwitchBot):
       else:
         print("Daddy isn't live ;(")
         ast.literal_eval(data)
-        print(type(data["is_live"]))
+        print(data["is_live"])
         data["is_live"] = "False"
       with open('communication.json', 'w') as f:
         json.dump(data, f)
